2e23e9b0ec52a302b2cc03f1057bd0c6ff6b3437
Mahfuzur Rahman <m.rahman@ramom.de>
2022-08-02 04:16:27 +0200

    Added image using Oacker ; added packer image into terraform template

--- personal/terraform_code/main.tf
+++ personal/terraform_code/main.tf
@@ -3,15 +3,40 @@ provider "azurerm" {
   features {}
 }
 
-#in Udacity lab resource group is already available
-#resource "azurerm_resource_group" "Azuredevops" {
- # name     = "${var.prefix}-resources"
-  #location = var.location
-#}
+
 resource "azurerm_resource_group" "Azuredevops" {
    name =  "Azuredevops"
-   location = "East US"
+   location = "South Central US"
 }
+
+
+data "azurerm_platform_image" "ubunti1804LTS" {
+  location  = azurerm_resource_group.Azuredevops.location
+  publisher = "Canonical"
+  offer     = "UbuntuServer"
+  sku       = "18.04-LTS"
+}
+
+resource "azurerm_network_security_group" "Azuredevops" {
+  name                = "Azuredevops-NSG"
+  location            = "${azurerm_resource_group.Azuredevops.location}"
+  resource_group_name = "${azurerm_resource_group.Azuredevops.name}"
+
+  security_rule {
+    name                       = "allow_SSHib"
+    description                = "Allow SSH access"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+
 
 resource "azurerm_virtual_network" "Azuredevops" {
   name                = "${var.prefix}-network"
@@ -26,7 +51,17 @@ resource "azurerm_subnet" "internal" {
   virtual_network_name = azurerm_virtual_network.Azuredevops.name
   address_prefixes     = ["10.0.2.0/24"]
 }
+resource "azurerm_public_ip" "Azuredevops" {
+  name                = "TestPublicIp1"
+  resource_group_name = azurerm_resource_group.Azuredevops.name
+  location            = azurerm_resource_group.Azuredevops.location
+  allocation_method   = "Static"
 
+  tags = {
+    environment = "Production"
+    project= "project01"  
+  }
+}
 resource "azurerm_network_interface" "Azuredevops" {
   name                = "${var.prefix}-nic"
   resource_group_name = azurerm_resource_group.Azuredevops.name
@@ -36,20 +71,15 @@ resource "azurerm_network_interface" "Azuredevops" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = count.index == 1 ? azurerm_public_ip.Azuredevops.id : null
     
   }
 }
-resource "azurerm_public_ip" "Azuredevops" {
-  name                = "TestPublicIp1"
-  resource_group_name = azurerm_resource_group.Azuredevops.name
-  location            = azurerm_resource_group.Azuredevops.location
-  allocation_method   = "Static"
 
-  tags = {
-    environment = "Production"
-  }
+resource "tls_private_key" "Azuredevops" {
+  algorithm   = "RSA"
+  rsa_bits    = 4096
 }
-
 resource "azurerm_linux_virtual_machine" "Azuredevops" {
   name                            = "${var.prefix}-vm"
   resource_group_name             = azurerm_resource_group.Azuredevops.name
@@ -61,16 +91,23 @@ resource "azurerm_linux_virtual_machine" "Azuredevops" {
   network_interface_ids = [
     azurerm_network_interface.Azuredevops.id,
   ]
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
+admin_ssh_key {
+    username   = "${var.username}"
+    public_key = tls_private_key.Azuredevops.public_key_openssh
   }
+ source_image_reference {
+    publisher = data.azurerm_platform_image.ubunti1804LTS.publisher
+    offer     = data.azurerm_platform_image.ubunti1804LTS.offer
+    sku       = data.azurerm_platform_image.ubunti1804LTS.sku
+    version   = data.azurerm_platform_image.ubunti1804LTS.version
+  } 
 
   os_disk {
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
+  }
+   tags = {
+    environment = "Production"
+    project= "project01"
   }
 }
--- personal/terraform_code/vers.tf
+++ personal/terraform_code/vers.tf
@@ -1,15 +1,18 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
+  default = "project01"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
-  default = "East US"
+  default = "South Central US"
 }
 variable "username" {
   description = "The Azure VM login user name"
+  default = "mahfuzmr"
  
 }
 variable "password" {
    description = "The Azure VM login user password"
+   default = "P@ssword01"
 }
